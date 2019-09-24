7616f2a1a31495512e393a04d7ba8a8826c691de
buithitam <buitam2708@gmail.com>
2019-09-24 14:26:33 +0700

    test
    test

--- PlantsVsZombiesGame/src/com/t3h/gui/ZombieFrame.java
+++ PlantsVsZombiesGame/src/com/t3h/gui/ZombieFrame.java
@@ -2,8 +2,10 @@ package com.t3h.gui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
-public class ZombieFrame extends JFrame {
+public class ZombieFrame extends JFrame implements MouseListener {
     public static final int W_FRAME = 1120;
     public static final int H_FRAME = 630;
 
@@ -15,5 +17,33 @@ public class ZombieFrame extends JFrame {
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         ZombiePanel zombiePanel = new ZombiePanel();
         add(zombiePanel);
+        addMouseListener(this);
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        Graphics g = getGraphics();
+        g.setColor(Color.RED);
+        g.fillOval(e.getX(), e.getY(), 30, 30);
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
     }
 }
---
+++ PlantsVsZombiesGame/src/test/MouseMotionListenerExample2.java
@@ -0,0 +1,39 @@
+package test;
+import com.t3h.model.ImageLoader;
+
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+
+public class MouseMotionListenerExample2 extends Frame
+        implements MouseMotionListener {
+    Image counter = ImageLoader.getImage("Counter.png",getClass());
+    Label label;
+    Color color = Color.BLUE;
+
+    public MouseMotionListenerExample2() {
+        label = new Label();
+        label.setBounds(20, 40, 100, 20);
+        add(label);
+
+        addMouseMotionListener(this);
+
+        setSize(400, 400);
+        setLayout(null);
+        setVisible(true);
+    }
+
+    public void mouseDragged(MouseEvent e) {
+        label.setText("X=" + e.getX() + ", Y=" + e.getY());
+        Graphics g = getGraphics();
+        g.drawImage(counter,e.getX(), e.getY(),null);
+    }
+
+    public void mouseMoved(MouseEvent e) {
+        label.setText("X=" + e.getX() + ", Y=" + e.getY());
+    }
+
+    public static void main(String[] args) {
+        new MouseMotionListenerExample2();
+    }
+}
